9f607418b5387fb169a009ad50d5f49c1cbde9dc
RexYe <894450343@qq.com>
2018-04-16 11:10:35 +0800

    new sis

---
+++ ScientificInformationSystem/sis/src/main.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import App from './App'
+import Vuex from 'vuex'
+import VueRouer from 'vue-router'
+import line from 'components/line/line'
+import {DatePicker} from 'element-ui'
+
+Vue.component(DatePicker.name, DatePicker)
+
+Vue.use(VueRouer)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    color: ['#325B69', '#698570', '#AE5548', '#6D9EA8', '#9CC2B0', '#C98769']
+  }
+});
+const router = new VueRouer({
+  routes: [{
+    path: '/',
+    component: line
+  }],
+  linkActiveClass: 'active'
+})
+new Vue({
+  router,
+  store,
+  template: '<App>',
+  components: {
+    App
+  },
+  data: {
+    eventHub: new Vue(),
+    charts: []
+  }
+}).$mount('#app')
+
+// router.push('line')
