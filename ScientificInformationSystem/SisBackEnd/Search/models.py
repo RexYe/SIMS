41088e27fd32b66169d185938bafa2c5ffe29dac
RexYe <894450343@qq.com>
2018-04-19 11:35:33 +0800

    new authors table

--- ScientificInformationSystem/SisBackEnd/Search/models.py
+++ ScientificInformationSystem/SisBackEnd/Search/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class authors(models.Model):
+    uniid = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    sex = models.CharField(max_length=30)
+    organization = models.CharField(max_length=30)
+    def __unicode__(self):
+        return self.authors
